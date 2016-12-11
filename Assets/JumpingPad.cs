f2be3a00f18859835c4706bc4cd026d4ea01adeb
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2016-12-12 00:34:52 +0900

    init

---
+++ Assets/JumpingPad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpingPad : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+    void OnTriggerEnter () {
+        print("hello");
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce(0, 10, 0, ForceMode.Impulse);
+        //rigidbody.isKinematic = true;
+    }
+}
---
+++ Assets/Script/KillScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour {
+
+    private Vector3 checkpoint;
+	// Use this for initialization
+	void Start () {
+        checkpoint = transform.position;
+        checkpoint.x = -76.6f;
+        checkpoint.y = -38.73f;
+        checkpoint.z = 65.8f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        print("DEAD");
+        Vector3 tmp = transform.position;
+        tmp.x = -76.6f;
+        tmp.y = -38.73f;
+        tmp.z = 65.8f;
+        transform.position = tmp;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Falling Platform Trigger" && other.name != "JumpPadTrigger")
+        {
+            print("DEAD : " + other);
+            transform.position = checkpoint;
+        }
+    }
+}
