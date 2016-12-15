7e8e7b668a9a12a3e035ed7bd458e69a0c742cad
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2016-12-15 17:36:09 +0900

    basic menu

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class InputManager : MonoBehaviour {
+
+    Dictionary<string, KeyCode> buttonKeys;
+
+    void OnEnable()
+    {
+        buttonKeys = new Dictionary<string, KeyCode>();
+
+        // TODO:  Consider reading these from a user preferences file
+        buttonKeys["Up"] = KeyCode.W;
+        buttonKeys["Down"] = KeyCode.S;
+        buttonKeys["Left"]  = KeyCode.A;
+        buttonKeys["Right"] = KeyCode.D;
+        buttonKeys["Jump"] = KeyCode.Space;
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public bool GetButtonDown( string buttonName )
+    {
+        // TODO: Check to see if the game is supposed to be paused
+        //  Or maybe if you're in a different input mode (like a window
+        //  is open, or if the player is typing in a text box)
+
+        if( buttonKeys.ContainsKey(buttonName) == false )
+        {
+            Debug.LogError("InputManager::GetButtonDown -- No button named: " + buttonName);
+            return false;
+        }
+
+        return Input.GetKeyDown( buttonKeys[buttonName] );
+    }
+
+    public string[] GetButtonNames()
+    {
+        return buttonKeys.Keys.ToArray();
+    }
+
+    public string GetKeyNameForButton( string buttonName )
+    {
+        if( buttonKeys.ContainsKey(buttonName) == false )
+        {
+            Debug.LogError("InputManager::GetKeyNameForButton -- No button named: " + buttonName);
+            return "N/A";
+        }
+
+        return buttonKeys[buttonName].ToString();
+    }
+
+    public void SetButtonForKey( string buttonName, KeyCode keyCode )
+    {
+        buttonKeys[buttonName] = keyCode;
+    }
+}
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    InputManager inputManager;
+    private string menu = "Main";
+    string buttonToRebind = null;
+
+    void Start()
+    {
+        inputManager = GameObject.FindObjectOfType<InputManager>();
+
+        // Create one "Key List Item" per button in inputManager
+    }
+
+    void OnGUI()
+    {
+        if (menu.Equals("Main"))
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 100, 150, 50), "Play"))
+            {
+                SceneManager.LoadScene("Kitchen");
+                //menu = "Play";
+            }
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 25, 150, 50), "Shortcuts"))
+            {
+                menu = "Shortcuts";
+            }
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 + 150, 150, 50), "Quit Game"))
+            {
+                Application.Quit();
+            }
+        }
+
+        if (menu.Equals("Play"))
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 100, 150, 50), "Level_1"))
+            {
+                //Application.LoadLevel("Play");
+                menu = "Play";
+            }
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 + 150, 150, 50), "Return"))
+            {
+                menu = "Main";
+            }
+        }
+
+        if (menu.Equals("Shortcuts"))
+        {
+
+            GUI.Box(new Rect(Screen.width / 2 + 25, Screen.height / 2 - 170, 100, 50), inputManager.GetKeyNameForButton("Up"));
+            if (GUI.Button(new Rect(Screen.width / 2 - 125, Screen.height / 2 - 170, 100, 50), "Up"))
+            {
+                //StartRebindFor("Up");
+            }
+
+            GUI.Box(new Rect(Screen.width / 2 + 25, Screen.height / 2 - 110, 100, 50), inputManager.GetKeyNameForButton("Down"));
+            if (GUI.Button(new Rect(Screen.width / 2 - 125, Screen.height / 2 - 110, 100, 50), "Down"))
+            {
+                //StartRebindFor("Down");
+            }
+
+            GUI.Box(new Rect(Screen.width / 2 + 25, Screen.height / 2 - 50, 100, 50), inputManager.GetKeyNameForButton("Right"));
+            if (GUI.Button(new Rect(Screen.width / 2 - 125, Screen.height / 2 - 50, 100, 50), "Right"))
+            {
+                //StartRebindFor("Right");
+            }
+
+            GUI.Box(new Rect(Screen.width / 2 + 25, Screen.height / 2 + 10, 100, 50), inputManager.GetKeyNameForButton("Left"));
+            if (GUI.Button(new Rect(Screen.width / 2 - 125, Screen.height / 2  + 10, 100, 50), "Left"))
+            {
+                //StartRebindFor("Left");
+            }
+
+            GUI.Box(new Rect(Screen.width / 2 + 25, Screen.height / 2 + 75, 100, 50), inputManager.GetKeyNameForButton("Jump"));
+            if (GUI.Button(new Rect(Screen.width / 2 - 125, Screen.height / 2 + 75, 100, 50), "Jump"))
+            {
+               // StartRebindFor("Jump");
+            }
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 + 150, 150, 50), "Return"))
+            {
+                menu = "Main";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (buttonToRebind != null)
+        {
+            if (Input.anyKeyDown)
+            {
+                // WHICH key was pressed down?!?
+
+                // Loop through all possible keys and see if it was pressed down
+                foreach (KeyCode kc in System.Enum.GetValues(typeof(KeyCode)))
+                {
+                    // Is this key down?
+                    if (Input.GetKeyDown(kc))
+                    {
+                        // Yes!
+                        inputManager.SetButtonForKey(buttonToRebind, kc);
+ //                       buttonToLabel[buttonToRebind].text = kc.ToString();
+                        buttonToRebind = null;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    void StartRebindFor(string buttonName)
+    {
+        Debug.Log("StartRebindFor: " + buttonName);
+
+        buttonToRebind = buttonName;
+    }
+}
